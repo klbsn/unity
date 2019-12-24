d8806186f0a56c4e2a18f92fa38cbde9062bc372
ktwo <klbushan@gmail.com>
2019-12-24 23:09:24 +0530

    game update
    unity game update

---
+++ ktwo/Assets/scripts/playerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private CharacterController playerController;
+    public float playerSpeed;
+    private Vector3 movePlayer;
+    private float verticalVelocity = 0.0f;
+    private float gravity = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer = Vector3.zero;
+        if(playerController.isGrounded){
+            verticalVelocity = 0.02f;
+
+        }else{
+            verticalVelocity -= gravity * Time.deltaTime;
+
+        }
+        movePlayer.x = Input.GetAxis("Horizontal") * playerSpeed;
+        movePlayer.y = verticalVelocity;
+        movePlayer.z = playerSpeed;
+        playerController.Move (movePlayer * Time.deltaTime);
+        
+    }
+}
